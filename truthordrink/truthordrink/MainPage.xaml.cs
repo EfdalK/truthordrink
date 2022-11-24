af553b27015b67e75185f8200ade544b066ffe2c
EfdalKislali <94622186+EfdalKislali@users.noreply.github.com>
2022-11-24 12:20:38 +0100

    L: W1 Login, Main page, pagina's toevoegen

--- truthordrink/truthordrink/MainPage.xaml.cs
+++ truthordrink/truthordrink/MainPage.xaml.cs
@@ -14,5 +14,24 @@ namespace truthordrink
         {
             InitializeComponent();
         }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isUsernameEmpty = string.IsNullOrEmpty(UsernameEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (isUsernameEmpty)
+            {
+                UsernameEntry.Placeholder = "User niet leeglaten";
+            }
+            else if (isPasswordEmpty)
+            {
+                PasswordEntry.Placeholder = "Pass niet leeglaten";
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
     }
 }
